f4b3719a16ed8bfff69bdedf2e145e47385a18ee
yoctta <zhq2015@mail.ustc.edu.cn>
2021-07-05 20:02:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,22 @@ import torch
 import subprocess
 #torch.autograd.set_detect_anomaly(True)
 
+def pretrain():
+    name='Efb4'
+    url='tcp://127.0.0.1:27015'
+    Config=train_config(name,['ff-all-c23','efficientnet-b4'],url=url,attention_layer='b5',feature_layer='logits',epochs=20,batch_size=16,AGDA_loss_weight=0)
+    Config.mkdirs()
+    distributed_train(Config) 
+    procs=[subprocess.Popen(['/bin/bash','-c','CUDA_VISIBLE_DEVICES={} python main.py test {} {}'.format(i,name,j)]) for i,j in enumerate(range(-3,0))]
+    for i in procs:
+        i.wait()
+
+## do pretrain first!
 def aexp():
     name='a1_b5_b2'  
-    url='tcp://127.0.0.1:27015'
-    Config=train_config(name,['ff-all-c23','efficientnet-b4'],url=url,attention_layer='b5',feature_layer='b2',epochs=20,batch_size=15,\
-        ckpt='checkpoints/a1_b5_b7/ckpt_35.pth',inner_margin=[0.2,-0.8],margin=0.8)
+    url='tcp://127.0.0.1:27016'
+    Config=train_config(name,['ff-all-c23','efficientnet-b4'],url=url,attention_layer='b5',feature_layer='b2',epochs=50,batch_size=15,\
+        ckpt='checkpoints/Efb4/ckpt_19.pth',inner_margin=[0.2,-0.8],margin=0.8)
     Config.mkdirs()
     distributed_train(Config) 
     procs=[subprocess.Popen(['/bin/bash','-c','CUDA_VISIBLE_DEVICES={} python main.py test {} {}'.format(i,name,j)]) for i,j in enumerate(range(-3,0))]
